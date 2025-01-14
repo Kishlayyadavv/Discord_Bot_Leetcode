2bd605541df342a64f09b44f4cdd5eda9392f064
Kishlay Yadav <151813217+Kishlayyadavv@users.noreply.github.com>
2025-01-14 21:06:56 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-DISCORD_TOKEN="MTMyODM0MDgxNTc4ODkwMDQxNA.GRfz1M.0R4OXQhqyVdlLelRHZ-JWLSrkbZ5NJ8NDE6rog"
-MONGO_PASSWORD="LeetCode_Bot@123"
-MONGO_URI="mongodb+srv://LeetCode_Bot:qd5bsddti1UQ7Twu@leetcodebot.ezppu.mongodb.net/?retryWrites=true&w=majority&appName=LeetCodeBot"
-NOTIFICATION_CHANNEL_ID = 1328628822756823065  # Replace with your actual channel ID
-
-
